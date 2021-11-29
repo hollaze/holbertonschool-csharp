1a41246f2fc0ec9ef3e6a8d7700f3e410feb8381
hollaze <deperiers.a@gmail.com>
2021-11-29 16:58:55 +0100

    check

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,11 +5,9 @@ class Program
         static void Main(string[] args)
         {
                 string str = "Holberton School";
-
                 for (int i = 0; i < 3; i++)
                         Console.Write(str);
                 Console.WriteLine();
-
                 for (int i = 0; i < 9; i++)
                         Console.Write(str[i]);
                 Console.WriteLine();
