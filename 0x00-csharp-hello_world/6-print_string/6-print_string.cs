956dacd9675982fa78d657eac5afca87ff08d213
hollaze <deperiers.a@gmail.com>
2021-11-29 17:32:48 +0100

    print string multiples times without conditions nor loops

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,11 +5,11 @@ class Program
         static void Main(string[] args)
         {
                 string str = "Holberton School";
-                for (int i = 0; i < 3; i++)
-                        Console.Write(str);
-                Console.WriteLine();
-                for (int i = 0; i < 9; i++)
-                        Console.Write(str[i]);
-                Console.WriteLine();
+
+                Console.Write(str);
+                Console.Write(str);
+                Console.WriteLine(str);
+
+                Console.WriteLine(str.Substring(0, 9));
         }
 }
