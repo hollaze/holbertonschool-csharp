ace6906592b0b70007c79067fa8ddfb8c40d5f0b
hollaze <deperiers.a@gmail.com>
2021-11-30 09:28:52 +0100

    print comb2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        for (int i = 0; i < 9; i++)
+                        {
+                                for (int j = i + 1; j <= 9; j++)
+                                {
+                                    Console.Write(string.Format((i == 8 && j == 9) ? "{0}{1}\n" : "{0}{1}, ", i, j));
+                                }
+                        }
+                }
+        }
+}
