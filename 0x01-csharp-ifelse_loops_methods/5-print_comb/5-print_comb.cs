f1faccf838f58f8e7c6f30eba0fb40f9064084c5
hollaze <deperiers.a@gmail.com>
2021-11-30 09:08:58 +0100

    print comb

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        for (int i = 0; i < 100; i++)
+                                Console.Write(string.Format((i != 99) ? "{0:D2}, " : "{0:D2}\n", i));
+                }
+    }
+}
