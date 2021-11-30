1a1a1dc417c334f08d708093d816d5b01002fab0
hollaze <deperiers.a@gmail.com>
2021-11-30 10:36:01 +0100

    print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+        public static void PrintDiagonal(int length)
+        {
+                for (int i = 0; i < length; i++)
+                {
+                        for (int reach = 0; reach <= i; reach++)
+                        {
+                                if (reach == i)
+                                        Console.WriteLine('\\');
+                                else
+                                        Console.Write(' ');
+                        }
+
+                }
+
+                Console.WriteLine();
+        }
+}
