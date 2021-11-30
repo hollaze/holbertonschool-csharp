8404ff3684874aad026dc7b05f5ca95b5a3f479c
hollaze <deperiers.a@gmail.com>
2021-11-30 10:48:24 +0100

    fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+                for (int i = 1; i <= 100; i++)
+                {
+                        if ((i % 3) == 0 && (i % 5) == 0)
+                                Console.Write("FizzBuzz ");
+			else if (i % 3 == 0)
+                                Console.Write("Fizz ");
+			else if (i % 5 == 0)
+                                Console.Write((i != 100) ? "Buzz " : "Buzz\n");
+			else
+                                Console.Write($"{i} ");
+                }
+        }
+}
