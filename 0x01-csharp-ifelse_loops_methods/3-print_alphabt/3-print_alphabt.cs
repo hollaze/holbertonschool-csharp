0e3196e49a40f922212ceebe30d1964fd2238e31
hollaze <deperiers.a@gmail.com>
2021-11-30 08:55:31 +0100

    alphabet lowercase without q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        for (char a = 'a'; a <= 'z'; a++)
+                        {
+                                if (a != 'q' && a != 'e')
+                                        Console.Write(a);
+                        }
+                }
+        }
+}
