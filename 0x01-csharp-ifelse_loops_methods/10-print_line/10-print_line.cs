d46e7710a2310aa9c9cc8d2e6473e3e6dc0446b0
hollaze <deperiers.a@gmail.com>
2021-11-30 10:17:42 +0100

    print line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+        public static void PrintLine(int length)
+        {
+                int i = 0;
+                while (i < length)
+                {
+                        Console.Write("_");
+                        i++;
+                }
+                Console.WriteLine();
+        }
+}
