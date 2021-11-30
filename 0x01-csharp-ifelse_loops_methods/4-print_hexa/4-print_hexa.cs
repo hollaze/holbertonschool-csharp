a3c18e64e519ce230c61b85e9ee88ac0fcdf8f43
hollaze <deperiers.a@gmail.com>
2021-11-30 09:01:18 +0100

    decimal and hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        for (int number = 0; number < 99; number++)
+                                Console.WriteLine(string.Format("{0} = 0x{0:x}", number));
+                }
+    }
+}
