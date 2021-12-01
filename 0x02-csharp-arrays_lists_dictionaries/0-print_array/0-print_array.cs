e3ce64a0104cd794658a48dbc0fe134bc52a00b7
hollaze <deperiers.a@gmail.com>
2021-12-01 11:25:16 +0100

    print size of an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] array;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        array = new int[size];
+
+        if (size == 0)
+            Console.WriteLine();
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write((i < size - 1) ? $"{i} " : $"{i}\n");
+        }
+
+        return (array);
+    }
+}
