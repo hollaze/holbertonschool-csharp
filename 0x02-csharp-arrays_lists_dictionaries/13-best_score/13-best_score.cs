3ebc5b2518dc7ebcb58556141c562f3893976e00
hollaze <deperiers.a@gmail.com>
2021-12-01 20:37:44 +0100

    best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxValue;
+
+        maxValue = myList.Values.Max();
+
+        foreach (string key in myList.Keys)
+        {
+            if (myList[key] == maxValue)
+                return (key);
+        }
+        return ("None");
+    }
+}
