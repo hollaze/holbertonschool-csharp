18559004170806ecb01cd8ecf3857ca955dfd67f
hollaze <deperiers.a@gmail.com>
2021-12-01 20:48:21 +0100

    rectangular array print

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == 2 && j == 2)
+                    Console.Write("1 ");
+                else
+                    Console.Write(j == 4 ? "0" : "0 ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
