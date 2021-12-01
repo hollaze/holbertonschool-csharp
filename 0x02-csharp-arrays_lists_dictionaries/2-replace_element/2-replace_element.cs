9d621ebfd0afa0ed19c11c5900ceac4c537e43e7
hollaze <deperiers.a@gmail.com>
2021-12-01 12:20:37 +0100

    modifiying out of range condition

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index < 0 || index > int.MaxValue)
+        if (index < 0 || index > array.Length)
         {
             Console.WriteLine("Index out of range");
             return (array);
