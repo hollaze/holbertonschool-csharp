a9e6302a7fcd4d9c0b01041368a53cff5bcab95c
hollaze <deperiers.a@gmail.com>
2021-12-01 12:15:10 +0100

    print array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+            Console.WriteLine();
+        else
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(i > 0 ? "{0} " : "{0}\n", array[i]);
+            }
+        }
+    }
+}
