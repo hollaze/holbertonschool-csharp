c73c61e180b645207788ec605120ff2f4a0689ed
hollaze <deperiers.a@gmail.com>
2021-12-01 19:14:03 +0100

    add key value

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,6 +5,14 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
+        // Replace value if key exists
+        if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+            return (myDict);
+        }
+
+        // create key value pair
         myDict.Add(key, value);
         return (myDict);
     }
