ce03aca626579b4d55811ef7cca46a4b0cd5a846
hollaze <deperiers.a@gmail.com>
2021-12-01 15:32:16 +0100

    print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> values;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        values = new List<int>(size);
+
+        if (size == 0)
+            Console.WriteLine();
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write((i < size - 1) ? $"{i} " : $"{i}\n");
+            values.Add(i);
+        }
+
+        return (values);
+    }
+}
