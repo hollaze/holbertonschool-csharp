c6decb87b8055c2b201fcf5f820fd9f797149edb
hollaze <deperiers.a@gmail.com>
2021-12-01 18:33:41 +0100

    count number of keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keyNumber = 0;
+
+        foreach (string key in myDict.Keys)
+            keyNumber += 1;
+
+        return (keyNumber);
+    }
+}
