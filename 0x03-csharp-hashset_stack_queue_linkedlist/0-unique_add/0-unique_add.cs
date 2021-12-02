5f0859240c3314ef15ff6105d7273f3828929477
hollaze <deperiers.a@gmail.com>
2021-12-02 14:34:35 +0100

    add all uniques elements in list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Weird code
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+
+        myList.Sort();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i < myList.Count - 1)
+                if (myList[i] != myList[i + 1])
+                    sum += myList[i];
+        }
+        sum += myList[myList.Count - 1];
+        return (sum);
+    }
+}
