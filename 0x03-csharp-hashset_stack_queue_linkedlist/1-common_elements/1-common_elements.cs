88201688fc4d8fbbd6ea924010099c7469cf133d
hollaze <deperiers.a@gmail.com>
2021-12-02 15:59:55 +0100

    common elements between two lists and sorted

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+
+        foreach (int value in list1)
+        {
+            foreach (int v in list2)
+            {
+                if (!newList.Contains(value) && !newList.Contains(v) && v == value)
+                    newList.Add(v);
+            }
+        }
+
+        newList.Sort();
+
+        return (newList);
+    }
+}
