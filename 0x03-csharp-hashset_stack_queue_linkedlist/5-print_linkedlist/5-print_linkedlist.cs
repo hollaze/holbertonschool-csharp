67b608c533ac8d65d87ae4340717ca5006989c3d
hollaze <deperiers.a@gmail.com>
2021-12-03 05:41:24 +0100

    create and print

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        if (size < 0)
+            return (llist);
+
+        for (int i = 0; i < size; i++)
+            llist.AddLast(i);
+        foreach (int value in llist)
+            Console.WriteLine(value);
+
+        return (llist);
+    }
+}
