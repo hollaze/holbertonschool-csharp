d6a27ecf9c003d826d51cca669b7d2992714e757
hollaze <deperiers.a@gmail.com>
2021-12-03 11:35:39 +0100

    delete node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current;
+
+        current = myLList.First;
+
+        for (int i = 0; i < index; i++)
+            current = current.Next;
+
+        myLList.Remove(current);
+    }
+}
