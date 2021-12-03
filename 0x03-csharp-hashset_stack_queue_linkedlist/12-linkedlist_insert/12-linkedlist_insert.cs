a733e58f3c54bef517b30b0d6a2ab5c2e896758e
hollaze <deperiers.a@gmail.com>
2021-12-03 10:18:39 +0100

    insert node at wanted position

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current;
+
+        if (myLList == null)
+            return (null);
+
+        if (myLList.First == null)
+            myLList.AddFirst(n);
+
+        current = myLList.First;
+
+        while (current != null)
+        {
+            if (current.Value < n)
+                current = current.Next;
+            else
+            {
+                myLList.AddBefore(current, n);
+                break;
+            }
+        }
+
+        return (myLList.First);
+    }
+}
