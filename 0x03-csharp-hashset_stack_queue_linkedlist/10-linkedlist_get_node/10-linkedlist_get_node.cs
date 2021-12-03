501b8fcf0cdcf498a083be00029a5ed0cdd32f07
hollaze <deperiers.a@gmail.com>
2021-12-03 09:40:21 +0100

    get node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int count = 0;
+
+        foreach (int value in myLList)
+        {
+            if (count == n)
+                return (value);
+            count++;
+        }
+        return (0);
+    }
+}
