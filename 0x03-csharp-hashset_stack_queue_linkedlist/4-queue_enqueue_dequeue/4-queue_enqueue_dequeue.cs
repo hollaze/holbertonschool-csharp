6830350be88a27c8380b6ffe14edbfd848f5d61f
hollaze <deperiers.a@gmail.com>
2021-12-03 05:19:23 +0100

    doing same as 3

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,25 +5,43 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        bool containsSearch = false;
+        string topItem = "";
+        bool containsSearch = false, firstItem = false;
+        int count = 0, index = 0;
 
-        Console.WriteLine("Number of items: {0}", aQueue.Count);
-        if (aQueue.Count == 0)
+        foreach (string str in aQueue)
+        {
+            count += 1;
+            if (firstItem == false)
+            {
+                topItem = str;
+                firstItem = true;
+            }
+
+            if (str == search)
+            {
+                containsSearch = true;
+                index = count;
+            }
+        }
+
+        if (containsSearch == true)
+        {
+            for (int i = 0; i < index; i++)
+                aQueue.Dequeue();
+        }
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Number of items: {0}", count);
+        if (count == 0)
             Console.WriteLine("Queue is empty");
         else
-            Console.WriteLine("First item: {0}", aQueue.Peek());
+            Console.WriteLine("First item: {0}", topItem);
 
         if (aQueue.Contains(search))
             containsSearch = true;
 
-        if (aQueue.Contains(search))
-        {
-            for (int i = 0; i < aQueue.Count; i++)
-                aQueue.Dequeue();
-            Console.WriteLine("Queue contains \"{0}\": {1}", search, containsSearch);
-        }
-
-        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, containsSearch);
 
         return (aQueue);
     }
