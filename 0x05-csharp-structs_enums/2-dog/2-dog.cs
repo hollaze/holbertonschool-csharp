64c4061f4a301b72e20c270b0002be5533f90708
hollaze <deperiers.a@gmail.com>
2021-12-13 14:18:11 +0100

    struct constructor

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+enum Rating
+{
+Good,
+Great,
+Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string dogName, float dogAge, string ownerName, Rating dogRating)
+    {
+        name = dogName;
+        age = dogAge;
+        owner = ownerName;
+        rating = dogRating;
+    }
+}
