e24d579e87afc006bf133363bed05228418d99e8
hollaze <deperiers.a@gmail.com>
2021-12-13 15:28:00 +0100

    is the checker wrong ?

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -11,11 +11,11 @@ struct Dog
     public float age;
     public string owner;
     public Rating rating;
-    public Dog(string name, float age, string name, Rating rating)
+    public Dog(string name, float age, string owner, Rating rating)
     {
         this.name = name;
         this.age = age;
-        this.owner = name;
+        this.owner = owner;
         this.rating = rating;
     }
 
