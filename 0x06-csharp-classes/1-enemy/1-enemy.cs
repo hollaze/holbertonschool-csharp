f7b3335e24cb09f500ac8df16fea148b51592a9d
hollaze <deperiers.a@gmail.com>
2021-12-14 11:33:25 +0100

    declaring zombie health value

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Zombie class.</summary>
+    public class Zombie
+    {
+        /// <value>health of the zombie.</value>
+        public int health;
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
