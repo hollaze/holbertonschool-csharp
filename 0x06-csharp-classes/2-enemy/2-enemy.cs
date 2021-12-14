d6044d0d10156a350e0bc3f77e7b67007fbba7f2
hollaze <deperiers.a@gmail.com>
2021-12-14 11:46:44 +0100

    new constructor in zombie class

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Zombie class.</summary>
+    public class Zombie
+    {
+        /// <value>health of the zombie.</value>
+        public int health;
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="Zombie"/> class.
+        /// <param name="value">health value</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+    }
+}
