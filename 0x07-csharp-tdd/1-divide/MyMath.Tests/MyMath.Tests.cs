3f3962ee0c37f35ffe423294b3eb44aec6d5a80f
hollaze <deperiers.a@gmail.com>
2021-12-16 18:45:04 +0100

    test a 2D array

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using System;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class tests1
+        {
+            [Test]
+            public void TestIsDividedByZero()
+            {
+                int[,] newMatrix = new int[3, 3];
+                int num = 3;
+
+                newMatrix = new int[,] { { 18, -64, 30 }, { 78, 21, 0 }, { 100, -98, 65489 } };
+
+                newMatrix = MyMath.Matrix.Divide(newMatrix, num);
+                Assert.AreNotEqual(0, num);
+            }
+
+            [Test]
+            public void TestIsMatrixNull()
+            {
+                int[,] newMatrix = new int[0, 2];
+                int num = 3;
+
+                newMatrix = new int[,] { { 185694982, -64, 0 } };
+                newMatrix = MyMath.Matrix.Divide(newMatrix, num);
+
+                Assert.IsNotNull(newMatrix);
+            }
+
+            [Test]
+            public void TestGoodResult()
+            {
+                int[,] newMatrix = new int[0, 2];
+                int num = 3, val1 = 185694982, val2 = -64, val3 = 0;
+
+                newMatrix = new int[,] { { 185694982, -64, 0 } };
+                newMatrix = MyMath.Matrix.Divide(newMatrix, num);
+
+                Assert.AreEqual(val1 / num, newMatrix[0, 0]);
+                Assert.AreEqual(val2 / num, newMatrix[0, 1]);
+                Assert.AreEqual(val3 / num, newMatrix[0, 2]);
+            }
+
+            [Test]
+            public void TestIndexOutOfRange()
+            {
+                int[,] newMatrix = new int[0, 2];
+                int num = 3;
+
+                newMatrix = new int[,] { { 185694982, -64, 0 } };
+                newMatrix = MyMath.Matrix.Divide(newMatrix, num);
+
+                if (newMatrix.GetLength(0) > MyMath.Matrix.Divide(newMatrix, num).GetLength(0)
+                    || newMatrix.GetLength(1) > MyMath.Matrix.Divide(newMatrix, num).GetLength(1))
+                {
+                    throw new IndexOutOfRangeException("Index out of range");
+                    throw new ArgumentOutOfRangeException("Argument out of range");
+                }
+
+                Assert.Pass();
+            }
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Matrix class
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// Divide all elements in matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="num">divide the elements by num</param>
+        /// <returns></returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            int[,] newMatrix = matrix;
+
+            if (matrix == null)
+                return (null);
+
+            try
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        newMatrix[i, j] /= num;
+                    }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+            }
+
+            return (newMatrix);
+        }
+    }
+}
