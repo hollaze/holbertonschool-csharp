c4f9095306528d6229b2db02dbf75a102096f560
hollaze <deperiers.a@gmail.com>
2021-12-16 20:56:22 +0100

    adding usefull files

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
