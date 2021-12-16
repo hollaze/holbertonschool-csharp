37c14d976b1c52e9c12734770ceffde17ac3e9de
hollaze <deperiers.a@gmail.com>
2021-12-16 19:30:47 +0100

    test a 2D array

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -9,7 +9,7 @@ namespace MyMath.Tests
         public class tests1
         {
             [Test]
-            public void Test1()
+            public void TestIsDividedByZero()
             {
                 int[,] newMatrix = new int[3, 3];
                 int num = 3;
@@ -21,7 +21,7 @@ namespace MyMath.Tests
             }
 
             [Test]
-            public void Test2()
+            public void TestIsMatrixNull()
             {
                 int[,] newMatrix = new int[0, 2];
                 int num = 3;
@@ -33,7 +33,7 @@ namespace MyMath.Tests
             }
 
             [Test]
-            public void Test3()
+            public void TestGoodResult()
             {
                 int[,] newMatrix = new int[0, 2];
                 int num = 3, val1 = 185694982;
@@ -44,6 +44,33 @@ namespace MyMath.Tests
                 Assert.AreEqual(val1 / num, newMatrix[0, 0]);
             }
 
+            [Test]
+            public void TestIndexOutOfRange()
+            {
+                int[,] newMatrix = new int[0, 2];
+                int num = 3;
+
+                newMatrix = new int[,] { { 185694982, -64, 0 } };
+                newMatrix = MyMath.Matrix.Divide(newMatrix, num);
+
+                if (newMatrix.GetLength(0) > MyMath.Matrix.Divide(newMatrix, num).GetLength(0)
+                    || newMatrix.GetLength(1) > MyMath.Matrix.Divide(newMatrix, num).GetLength(1))
+                {
+                    throw new IndexOutOfRangeException("Index out of range");
+                    throw new ArgumentOutOfRangeException("Argument out of range");
+                }
+
+                Assert.Pass();
+            }
+
+            [Test]
+            public void TestType()
+            {
+                int[,] newMatrix = new int[0, 2];
+                int num = 3;
+
+                Assert.AreEqual(typeof(int[,]), MyMath.Matrix.Divide(newMatrix, num).GetType());
+            }
         }
     }
 }
