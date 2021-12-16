03b1b966e0a6707f3d05733276329037004a0cfb
hollaze <deperiers.a@gmail.com>
2021-12-16 20:54:59 +0100

    function check

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+                return (0);
+            nums.Sort();
+            return (nums[nums.Count - 2]);
+        }
+    }
+}
