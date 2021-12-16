60819ac10e3aeb2dc08093a3900dedc040bc36e6
hollaze <deperiers.a@gmail.com>
2021-12-16 21:24:38 +0100

    max integer in a list of integers

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -11,7 +11,7 @@ namespace MyMath
         /// <summary>
         /// Max integer in a list
         /// </summary>
-        /// <param name="nums"></param>
+        /// <param name="nums">list of integers</param>
         /// <returns>max integer in nums list</returns>
         public static int Max(List<int> nums)
         {
