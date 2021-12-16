a8dfbbc82502ff1aa02ad201f5253c4918a86fa6
hollaze <deperiers.a@gmail.com>
2021-12-16 09:26:51 +0100

    more tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace MyMath.Tests
 {
@@ -10,24 +11,29 @@ namespace MyMath.Tests
         public class tests1
         {
             [Test]
-            public void Test1()
+            public void goodResult()
             {
                 int a = 5, b = 11, res;
 
                 res = Operations.Add(a, b);
-                Assert.AreEqual(a+b, res);
+                Assert.AreEqual(a + b, res);
 
             }
             [Test]
-            public void Test2()
+            public void badResult()
             {
                 int a = 5, b = 11, res;
 
                 res = Operations.Add(a, b);
                 Assert.AreNotEqual(17, res);
             }
+            [Test]
+            public void type()
+            {
+                int a = 5, b = 11;
 
-
+                Assert.AreEqual(typeof(int), Operations.Add(a, b).GetType());
+            }
         }
     }
 }
