514f789764442a2d13844e1d2fe6e0e1f2491867
hollaze <deperiers.a@gmail.com>
2021-12-20 08:20:54 +0100

    is instance of

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj))
+            return (true);
+        return (false);
+    }
+}
