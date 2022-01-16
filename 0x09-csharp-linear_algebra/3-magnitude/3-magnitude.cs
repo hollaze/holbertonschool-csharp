4635ccceca4be8706bb6512c43f335ac47e38b8a
hollaze <deperiers.a@gmail.com>
2022-01-16 21:47:42 +0100

    magnitude of vectors

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Math Vectors.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Magnitude of a vector
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <returns>Vector magnitude</returns>
+    public static double Magnitude(double[] vector)
+    {
+        double magnitude = 0;
+
+        if (vector.Length <= 1)
+            return (-1);
+
+        if (vector.Length == 2)
+        {
+            magnitude = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+        }
+
+        else if (vector.Length == 3)
+        {
+            magnitude = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+        }
+
+        return (Math.Sqrt(magnitude));
+    }
+}
