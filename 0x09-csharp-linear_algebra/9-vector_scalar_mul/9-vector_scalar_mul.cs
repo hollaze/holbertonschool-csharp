d7e7aa873ba84cb0794f8f2cafedc0c0be690c62
hollaze <deperiers.a@gmail.com>
2022-01-16 22:23:49 +0100

    scale vectors

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Vector math
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Multiply vectors - scaling
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="scalar"></param>
+    /// <returns>Scaled vector</returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] scaledVector = new double[3];
+
+        if (vector.Length < 2 || vector.Length > 3)
+            return (-1);
+        
+        for (int i = 0; i < vector.Length; i++)
+            scaledVector[i] = vector[i] * scalar;
+        
+        return (scaledVector);
+    }
+}
+
