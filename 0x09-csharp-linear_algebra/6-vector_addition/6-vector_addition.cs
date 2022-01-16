de2e2b93ec5fde611f2197b2f2ccb306ca49cebf
hollaze <deperiers.a@gmail.com>
2022-01-16 22:10:17 +0100

    sum of two vectors

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Vector math
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Add two vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns>Sum of the two vectors</returns>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] vectorSum = new double[3];
+
+        if (vector1.Length < 2 || vector1.Length > 3
+            || vector2.Length < 2 || vector2.Length > 3
+            || vector1.Length != vector2.Length)
+            return (-1);
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            vectorSum[i] = vector1[i] + vector2[i];
+        }
+
+        return (vectorSum);
+    }
+}
