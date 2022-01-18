c8b941fff814f86fd523d23ae04831a37b63f1f4
hollaze <deperiers.a@gmail.com>
2022-01-18 19:50:09 +0100

    multiply matrix by a scalar number

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// Math Matrix
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Multiply matrix by a scalar number
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="scalar"></param>
+    /// <returns>Multiplied matrix</returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        // Check if matrix is 2D or 3D
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
+            matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+        {
+            return (new double[,] { { -1 } });
+        }
+
+        double[,] matrixRes = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrixRes[i, j] = matrix[i, j] * scalar;
+            }
+        }
+
+        return (matrixRes);
+    }
+}
