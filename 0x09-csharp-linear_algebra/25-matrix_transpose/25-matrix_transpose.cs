198272ad7cacaa7509f64a7bb32d0add745abb85
hollaze <deperiers.a@gmail.com>
2022-01-20 17:17:05 +0100

    transpose matrix

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Matrix Math
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Transpose matrix
+    /// </summary>
+    /// <param name="matrix">Matrix to transpose</param>
+    /// <returns>Transposed matrix</returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+            return (new double[0, 0]);
+        
+        double[,] matrixTransposed = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrixTransposed[row, col] = matrix[col, row];
+            }
+        }
+
+        return (matrixTransposed);
+    }
+}
