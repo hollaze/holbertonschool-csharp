6afe561f84e432a7ed56b92240c9c50084738adf
hollaze <deperiers.a@gmail.com>
2022-01-22 17:32:08 +0100

    how the fuck does it changes anything? [Fix] : adding double type to store value instead of double array and it changes the value of the rounding ... can someone explain ?

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -22,18 +22,20 @@ class MatrixMath
         double[,] matrixRotation2D = new double[,] { { cos, -sin },
                                                      { sin, cos } };
         double[,] matrixRes = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double storeNumber;
 
 
         for (int row = 0; row < matrix.GetLength(0); row++)
         {
             for (int col = 0; col < matrix.GetLength(1); col++)
             {
+                storeNumber = 0;
                 for (int i = 0; i < matrix.GetLength(0); i++)
                 {
-                    matrixRes[row, col] += matrixRotation2D[col, i] * matrix[row, i];
+                    storeNumber += matrixRotation2D[col, i] * matrix[row, i];
                 }
                 // 2 times round because it did not round up with 5 number
-                matrixRes[row, col] = Math.Round(Math.Round(matrixRes[row, col], 3), 2);
+                matrixRes[row, col] = Math.Round(storeNumber, 2);
             }
         }
 
