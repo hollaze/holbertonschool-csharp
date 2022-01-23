6bf2b881f7b416f492f7cb64b7b41b277e55c87f
hollaze <deperiers.a@gmail.com>
2022-01-23 19:04:32 +0100

    comment on why no 3x3 algo & changing determinant3x3Neg order

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -31,6 +31,9 @@ class MatrixMath
         // but doing determinant algorithm for NxN matrix is
         // too much for me, sorry for the one that sees this
         // (sorry for myself too ...)
+        
+        // Also doing the 3x3 algorithm the way I was thinking
+        // would have made the code way longer and way less clear
 
         // Positive and Negative
         double determinant3x3Pos = 0;
@@ -41,8 +44,8 @@ class MatrixMath
         determinant3x3Pos += (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]);
 
         determinant3x3Neg = matrix[0, 2] * matrix[1, 1] * matrix[2, 0];
-        determinant3x3Neg -= (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]);
         determinant3x3Neg -= (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
+        determinant3x3Neg -= (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]);
 
         return (Math.Round(determinant3x3Pos - determinant3x3Neg, 2));
     }
