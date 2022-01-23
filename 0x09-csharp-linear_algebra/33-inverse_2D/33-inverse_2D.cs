f8b950cf0a51027fabad6ec2b0af5783c0f116fe
hollaze <deperiers.a@gmail.com>
2022-01-23 20:50:23 +0100

    inverse 2D matrix

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -52,40 +52,6 @@ class DeterminantMatrix
 }
 
 /// <summary>
-/// Scalar Matrix
-/// </summary>
-class ScalarMatrix
-{
-    /// <summary>
-    /// Multiply matrix by a scalar number
-    /// </summary>
-    /// <param name="matrix"></param>
-    /// <param name="scalar"></param>
-    /// <returns>Multiplied matrix</returns>
-    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
-    {
-        // Check if matrix is 2D or 3D
-        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
-            matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
-        {
-            return (new double[,] { { -1 } });
-        }
-
-        double[,] matrixRes = new double[matrix.GetLength(0), matrix.GetLength(1)];
-
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                matrixRes[i, j] = matrix[i, j] * scalar;
-            }
-        }
-
-        return (matrixRes);
-    }
-}
-
-/// <summary>
 /// Math Matrix
 /// </summary>
 class MatrixMath
@@ -104,6 +70,16 @@ class MatrixMath
         }
         
         double inverseDeterminant = 1 / DeterminantMatrix.Determinant(matrix);
-        return (ScalarMatrix.MultiplyScalar(matrix, inverseDeterminant));  
+        double[,] newMatrix = newdouble[2, 2];
+        int i = 0;
+        
+        
+        foreach (double value in matrix)
+        {
+            newMatrix[i] = value * inverseDeterminant;
+            i++;
+        }
+        
+        return (newMatrix);
     }
 }
