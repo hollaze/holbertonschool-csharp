4dc84f251f747cceb2fdd7cd5729a29ba7479a30
hollaze <deperiers.a@gmail.com>
2022-03-21 16:31:35 +0100

    concatenate linked list string and char only

---
+++ 0x0A-csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// Generic Queue class
+/// </summary>
+class Queue<T>
+{
+    private int count;
+
+    /// <summary>Gets or sets the head node.</summary>
+    public Node head { get; set; }
+
+    /// <summary>Gets or sets the tail node.</summary>
+    public Node tail { get; set; }
+
+    /// <summary>Gets or sets a temporary node.</summary>
+    private Node temp { get; set; }
+
+
+    /// <summary>
+    /// Represent some generics nodes.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>Gets or sets the next node.</summary>
+        public Node next { get; set; }
+
+        /// <summary>Gets or sets the node value.</summary>
+        public T value { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="TValue">node value</param>
+        public Node(T TValue)
+        {
+            this.value = TValue;
+        }
+    }
+
+    /// <summary>
+    /// Check type of T.
+    /// </summary>
+    /// <returns>Queue's type</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+
+    /// <summary>
+    /// Creates a new Node and adds it to the end of the queue.
+    /// </summary>
+    /// <param name="TValue">Generic value of the node</param>
+    public void Enqueue(T TValue)
+    {
+        Node temp = new Node(TValue);
+
+        if (count == 0)
+        {
+            tail = temp;
+            head = temp;
+        }
+        else
+        {
+            tail.next = temp;
+            tail = temp;
+        }
+
+        this.count += 1;
+    }
+
+    /// <summary>
+    /// Removes the first node in the queue.
+    /// </summary>
+    /// <returns>Value of the first node</returns>
+    public T Dequeue()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            // default value
+            return default(T);
+        }
+
+        T headValue = head.value;
+        head = head.next;
+        this.count -= 1;
+        return headValue;
+    }
+
+    /// <summary>
+    /// Peek the first node of the queue without removing it.
+    /// </summary>
+    /// <returns>Value of the first node</returns>
+    public T Peek()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            // default value
+            return default(T);
+        }
+
+        T headValue = head.value;
+        return headValue;
+    }
+
+    /// <summary>
+    /// Print values of the linked list
+    /// </summary>
+    public void Print()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        temp = head;
+
+        while (temp != null)
+        {
+            Console.WriteLine(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    /// <summary>
+    /// Concatenate string or char of linked list
+    /// </summary>
+    /// <returns>Concatenated values</returns>
+    public string Concatenate()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return null;
+        }
+
+        temp = head;
+        StringBuilder concat = new StringBuilder();
+
+        if (typeof(T) == typeof(string) || typeof(T) == typeof(char))
+        {
+            while (temp != null)
+            {
+                concat.Append(temp.value);
+                if (typeof(T) == typeof(string) && temp.next != null)
+                    concat.Append(" ");
+                temp = temp.next;
+            }
+
+            return concat.ToString();
+        }
+
+        Console.WriteLine("Concatenate() is for a queue of Strings or Chars only");
+        return null;
+    }
+
+    /// <summary>
+    /// Count the number of nodes.
+    /// </summary>
+    /// <returns>Number of nodes</returns>
+    public int Count()
+    {
+        return this.count;
+    }
+}
