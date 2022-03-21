3218492bca373cc75237b3fef1d88f0126a9add4
hollaze <deperiers.a@gmail.com>
2022-03-21 09:33:42 +0100

    Check queue's type

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Generic class
+/// </summary>
+/// <typeparam name="T">Generic type</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Check type of T
+    /// </summary>
+    /// <returns>Queue's type</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+}
