70632d02fd2f8b3037e5a0c1dd1f8568bb016cb7
hollaze <deperiers.a@gmail.com>
2022-03-22 11:36:23 +0100

    Overriding .ToString() method

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+/// <summary>
+/// Base class.
+/// </summary>
+abstract class Base
+{
+    /// <summary>Gets or sets the name.</summary>
+    public string name { get; set; }
+    
+    /// <summary>
+    /// Override .ToString() method.
+    /// </summary>
+    /// <returns>Name and type.</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
