32dc53fd425ba8d6cf9434dc486ec12036651af5
hollaze <deperiers.a@gmail.com>
2022-03-23 09:13:40 +0100

    player health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+    
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
